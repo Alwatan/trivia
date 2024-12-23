8a7fc1268b59904591550708ee71fd5d3a96ca61
Alwatan <pugudean@gmail.com>
2024-12-23 11:14:36 +0300

    added data class for question

---
+++ app/src/main/java/tz/co/dbs/trivia/model/Question.kt
@@ -0,0 +1,10 @@
+package tz.co.dbs.trivia.model
+
+data class Question(
+    val A: String,
+    val B: String,
+    val C: String,
+    val D: String,
+    val answer: String,
+    val question: String
+)
